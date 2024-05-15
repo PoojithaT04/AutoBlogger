6510b38bed307ffd6de232092f5bb8ab9c08db1a
PoojithaT04 <138215826+PoojithaT04@users.noreply.github.com>
2024-05-15 19:59:53 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -9,7 +9,7 @@ import io
 def getLLamaresponse(input_text,no_words,blog_style):
 
     ### LLama2 model
-    llm=CTransformers(model=r'https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML/blob/main/llama-2-7b-chat.ggmlv3.q2_K.bin',
+    llm=CTransformers(model='https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML/blob/main/llama-2-7b-chat.ggmlv3.q2_K.bin',
                   model_type='llama',
                   config={'max_new_tokens':256,
                           'temperature':0.01})
